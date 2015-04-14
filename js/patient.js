89ad6dce2871c857de2762a3ff18a45b4d231ad3
Andres Hasfura <hasfuraa@mit.edu>
2015-04-13 23:03:59 -0400

    patient page progress

--- js/patient.js
+++ js/patient.js
@@ -4,34 +4,66 @@
 // [{  type: "SleepLog",
 // 	patient: "MarshallMathers",
 // 	dueDate: Datetime,
-// 	doctor: "Dr.Dre"
+// 	doctor: "Dr.Dre",
+//	html: ...
 // },
 // {	type: "EatingLog",
 // 	patient: "MarshallMathers",
 // 	dueDate: Datetime,
-// 	doctor: "Dr.Dre"
+// 	doctor: "Dr.Dre",
+//	html: ...
 // }
 // ]
 
-//Data Structures
-var forms = [];
+$(function() {
 
-//Remove form
-var removeForm = function(form) {
-	var i = forms.indexOf(form);
-	if(i != -1) {
-		forms.splice(i, 1);
-	}
-};
+	//Data Structures
+	var forms = [{
+					type:"Anxiety",
+					patient:"MarshallMathers",
+					dueDate:"Apr 15, 2015",
+					doctor:"Dr.Dre",
+					html:'<iframe src="https://docs.google.com/forms/d/1kDZ19hpSOj53k2yhiTATE--fsaoqgj8HUB-8CR9aOjc/viewform?embedded=true" width="500" height="500" frameborder="0" marginheight="0" marginwidth="0" id="anxietyForm" onsubmit="submitted=true;">Loading...</iframe>'
+				},{
+					type:"Sleep",
+					patient:"MarshallMathers",
+					dueDate:"Apr 11, 2015",
+					doctor:"Dr.Dre",
+					html:'<iframe src="https://docs.google.com/forms/d/10xn9Qp9T6goWqZsXCq6GEpThAPx4J1eW8M5338kGnW4/viewform?embedded=true" width="500" height="500" frameborder="0" marginheight="0" marginwidth="0" id="sleepForm" onsubmit="submitted=true;">Loading...</iframe>'
+				}];
 
-//Add form
-var addForm = function(form) {
-	forms.push(form);
-};
+	var submitted=false;
 
-//Sort form data Structure
-var sortFormsByTime = function() {
-	forms.sort(function(a,b) {
-		return Date.parse(a.dueDate) - Date.parse(b.dueDate);
+	$("#sleepForm").click(function(){
+		$("#form").html(forms[1].html);
+		$("#formName").html(forms[1].type);
+		alert(submitted);
 	});
-};
+
+	$("#anxietyForm").click(function(){
+		$("#form").html(forms[0].html);
+		$("#formName").html(forms[0].type);
+		alert(submitted);
+	});
+
+	//Remove form
+	var removeForm = function(form) {
+		var i = forms.indexOf(form);
+		if(i != -1) {
+			forms.splice(i, 1);
+		}
+	};
+
+	//Add form
+	var addForm = function(form) {
+		forms.push(form);
+	};
+
+	//Sort form data Structure
+	var sortFormsByTime = function() {
+		forms.sort(function(a,b) {
+			return Date.parse(a.dueDate) - Date.parse(b.dueDate);
+		});
+	};
+
+});
