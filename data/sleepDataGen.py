760067916535e09e8bac53b4e1d688ec2ac39837
Reinier Strobos <rstrobos@Reiniers-MacBook-Pro.local>
2015-04-15 21:30:12 -0400

    Graphs completely done

--- data/sleepDataGen.py
+++ data/sleepDataGen.py
@@ -9,13 +9,13 @@ date = 20141201
 #time_in_bed_rng = [9, 12]
 #fell_asleep_rng = [9, 12]
 f.write("TimeOfDay,WakeUpTime,TimeInBed,FellAsleep\n")
-
+# round(x*4)/4
 for i in range(data_pts):
-  wake_up = int(max(min(11, random.gauss(7,1.5)), 4))#random.uniform(wake_up_rng[0], wake_up_rng[1])
+  wake_up = round(max(min(11, random.gauss(7,1.5)), 4)*4)/4#random.uniform(wake_up_rng[0], wake_up_rng[1])
   
-  time_in_bed = int(max(min(24, random.gauss(22.5, 1.5)), 21))
+  time_in_bed = round(max(min(24, random.gauss(22.5, 1.5)), 21)*4)/4
   
-  fell_asleep = int(min(time_in_bed, max(min(24, random.gauss(22.5, 2)),  21)))
+  fell_asleep = round(min(time_in_bed, max(min(24, random.gauss(22.5, 2)),  21))*4)/4
   
   row_data = [ date, wake_up, time_in_bed, fell_asleep ]
   f.write(",".join([str(i) for i in row_data])+"\n")
