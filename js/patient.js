9995a1438cc9e848f7a0ca721a15b2d38b8a04f3
Andres Hasfura <hasfuraa@mit.edu>
2015-04-30 15:37:40 -0400

    updated patient page

--- js/patient.js
+++ js/patient.js
@@ -23,13 +23,19 @@ $(function() {
 					patient:"MarshallMathers",
 					dueDate:"Apr 15, 2015",
 					doctor:"Dr.Dre",
-					html:'<iframe src="https://docs.google.com/forms/d/1kDZ19hpSOj53k2yhiTATE--fsaoqgj8HUB-8CR9aOjc/viewform?embedded=true" width="500" height="500" frameborder="0" marginheight="0" marginwidth="0" id="anxietyForm" onsubmit="submitted=true;">Loading...</iframe>'
+					html:'<iframe src="https://docs.google.com/forms/d/1kDZ19hpSOj53k2yhiTATE--fsaoqgj8HUB-8CR9aOjc/viewform?embedded=true" width="100%" height="400" frameborder="0" marginheight="0" marginwidth="0" id="anxietyForm" onsubmit="submitted=true;">Loading...</iframe>'
 				},{
 					type:"Sleep",
 					patient:"MarshallMathers",
 					dueDate:"Apr 11, 2015",
 					doctor:"Dr.Dre",
-					html:'<iframe src="https://docs.google.com/forms/d/10xn9Qp9T6goWqZsXCq6GEpThAPx4J1eW8M5338kGnW4/viewform?embedded=true" width="500" height="500" frameborder="0" marginheight="0" marginwidth="0" id="sleepForm" onsubmit="submitted=true;">Loading...</iframe>'
+					html:'<iframe src="https://docs.google.com/forms/d/10xn9Qp9T6goWqZsXCq6GEpThAPx4J1eW8M5338kGnW4/viewform?embedded=true" width="100%" height="400" frameborder="0" marginheight="0" marginwidth="0" id="sleepForm" onsubmit="submitted=true;">Loading...</iframe>'
+				},{
+					type:"Mood",
+					patient:"MarshallMathers",
+					dueDate:"Apr 18, 2015",
+					doctor:"Dr.Dre",
+					html:'<iframe src="https://docs.google.com/forms/d/1Hk_DksNNvJ-OTJFhRNiVQhMroPHMcHi9PlMlElhqpV4/viewform?embedded=true" width="100%" height="400" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>'
 				}];
 
 	var submitted=false;
@@ -44,6 +50,12 @@ $(function() {
 		$("#form").html(forms[0].html);
 		$("#formName").html(forms[0].type);
 		//alert(submitted);
+	});
+
+	$("#moodForm").click(function(){
+		$("#form").html(forms[2].html);
+		$("#formName").html(forms[2].type);
+		alert('sorry, havent gotten around making the correct form quite yet 8===D');
 	});
 
 	//Remove form
