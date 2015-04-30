7d926ad21431ceae6118772dad3efb9974dbced0
chums923 <chumbleya@gmail.com>
2015-04-30 12:35:27 -0400

    + db.js, basic objects

---
+++ js/db.js
@@ -0,0 +1,43 @@
+var Patients = {
+	"MarshallMathers": {
+		"firstName": "Marshall",
+		"lastName": "Mathers",
+		"bio": {
+			"height": "5' "+ '10"',
+			"weight": "180",
+			"bloodPressure": "120/70",
+			"heartRate": "68",
+			"recentUpdates": "Marshall suffers from severe depression caused by his recent divorce.",
+			"overview": "Marshall has started taking medicine A on 12/6/2014."
+		},
+		"photo": "./images/marshall.jpg"
+	},
+	"JaneGoodall": {
+		"firstName": "Jane",
+		"lastName": "Goodall",
+		"bio": {
+			"height": "5' "+ '3"',
+			"weight": "110",
+			"bloodPressure": "110/70",
+			"heartRate": "58",
+			"recentUpdates": "Jane has sleep apnea",
+			"overview": "Jane was perscribed intranasal corticosteroid medicine on 12/25/2015. She has been adhering to the medicine rigorously."
+		},
+		"photo": "./images/janeGoodall.jpg"
+	}
+};
+
+var Doctors = {
+	"Dr. Dre": {
+
+	}
+};
+
+var basicBio = {
+	"height": "5' "+ '9"',
+	"weight": "150",
+	"bloodPressure": "120/80",
+	"heartRate": "72",
+	"recentUpdates": "None",
+	"overview": "None"
+};
