0500fc1d3a5e2c2e24ff1b5ba05ee75ef8a122d7
chums923 <chumbleya@gmail.com>
2015-05-02 13:16:02 -0400

    Buggy code

--- js/doctor.js
+++ js/doctor.js
@@ -272,8 +272,23 @@ var csvExport = function(dataFile, filename) {
         type: "GET",
         url: "./data"+dataFile,
         dataType: "text",
-        success: function(data) {
-            console.log(typeof data);
+        success: function(allText) {
+            var allTextLines = allText.split(/\r\n|\n/);
+    		var headers = allTextLines[0].split(',');
+    		var lines = [];
+
+    		for (var i=1; i<allTextLines.length; i++) {
+        		var data = allTextLines[i].split(',');
+        		if (data.length == headers.length) {
+            		var tarr = [];
+            		for (var j=0; j<headers.length; j++) {
+                		tarr.push(headers[j]+":"+data[j]);
+            		}
+            		lines.push(tarr);
+        		}
+    		}
+    		console.log(headers);
+    		console.log(lines);
         }
      });
 
