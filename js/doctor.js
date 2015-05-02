9315d15ceff47ff92011574682eb4e389dac089f
chums923 <chumbleya@gmail.com>
2015-05-02 13:23:21 -0400

    Buggy code

--- js/doctor.js
+++ js/doctor.js
@@ -274,20 +274,11 @@ var csvExport = function(dataFile, filename) {
         dataType: "text",
         success: function(allText) {
             var allTextLines = allText.split(/\r\n|\n/);
-    		var headers = allTextLines[0].split(',');
     		var lines = [];
-
-    		for (var i=1; i<allTextLines.length; i++) {
+    		for (var i=0; i<allTextLines.length; i++) {
         		var data = allTextLines[i].split(',');
-        		if (data.length == headers.length) {
-            		var tarr = [];
-            		for (var j=0; j<headers.length; j++) {
-                		tarr.push(headers[j]+":"+data[j]);
-            		}
-            		lines.push(tarr);
-        		}
+        		lines.push(data);
     		}
-    		lines.unshift(headers);
     		lines.forEach(function(infoArray, index){
             	dataString = infoArray.join(",");
             	csvContent += dataString+ "\n";
