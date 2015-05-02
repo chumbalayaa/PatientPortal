543905a4d986ad65d65592444c8ee9cf2996decf
Karson Ota <klota@mit.edu>
2015-05-02 13:10:37 -0400

    bug

--- js/doctor.js
+++ js/doctor.js
@@ -126,7 +126,6 @@ var loadLeftPanel = function(fullName) {
 		context: document.body,
 		success: function(response) {
 			$("#leftPanel").html(response);
-			console.log(fullName);
 			populateBio(fullName);
 		}
 	});
