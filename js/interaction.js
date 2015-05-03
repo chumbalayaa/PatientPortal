13398a31a802d795ab4f8d5fa62224faa6b74914
Andres Hasfura <hasfuraa@mit.edu>
2015-05-03 15:08:02 -0400

    w

---
+++ js/interaction.js
@@ -0,0 +1,34 @@
+$(function() {
+
+	alert('buttholes');
+
+	$('span').click(function(){
+		alert('hi0');
+	});
+
+	$('input').click(function(){
+		alert('hi1');
+	});
+
+	$('label').click(function(){
+		alert('hi2');
+	});
+
+	$('ul').click(function(){
+		alert('hi3');
+	});
+
+	$('li').click(function(){
+		alert('hi4');
+	});
+
+	$('#test').click(function(){
+		alert('hi4');
+	});
+
+	$('.completedForm').mouseover(function(){
+		console.log('hi');
+	});
+
+});
+
