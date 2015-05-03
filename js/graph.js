fbe5786a7aea028ab167a654e4711873bc22b34a
Reinier Strobos <rstrobos@Reiniers-MacBook-Pro.local>
2015-05-03 19:58:39 -0400

    annotation

--- js/graph.js
+++ js/graph.js
@@ -83,11 +83,13 @@ function drawGraph(firstName, lastName,  graphType, startDate, endDate, minDate,
           } };
     // }
     var annotations = [[parseDate("20141213"), "<b>".concat(parseDate("20141213").toDateString()).concat("</b><br>Marshall started taking Xanax.")]]//[[date, html],...]
-    for (var i = 0; i < Patients[capitalizeFirstLetter(firstName).concat(capitalizeFirstLetter(lastName))]['annotations'][graphType].length; i++){
-        annotations.push(Patients[capitalizeFirstLetter(firstName).concat(capitalizeFirstLetter(lastName))]['annotations'][graphType][i])
+    var fullName = capitalizeFirstLetter(firstName).concat(capitalizeFirstLetter(lastName));
+    console.log(fullName);
+    for (var i = 0; i < Patients[fullName]['annotations'][graphType].length; i++){
+        annotations.push(Patients[fullName]['annotations'][graphType][i])
     }
 
-    
+
     //
     var main_x = d3.time.scale()
         .range([0, main_width]);
